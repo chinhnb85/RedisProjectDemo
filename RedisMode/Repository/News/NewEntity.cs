3e90e04b0c75979a65b7a7fd045cbd082dc36444
chinhnb <bachinh2105@gmail.com>
2016-04-21 17:16:48 +0700

    ok

---
+++ RedisMode/Repository/News/NewEntity.cs
@@ -0,0 +1,10 @@
+﻿using RedisMode.Base;
+
+namespace RedisMode.Repository.News
+{
+    public class NewsEntity:Entity
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+    }
+}
